512bfe4a753c5b59d79e2c083c176d796ce20ec8
ViewShumoon <ViewShumoon@outlook.com>
2024-06-09 16:29:20 +0800

    添加 功能快捷键 F8

--- RefreshMarket/RefreshMarketPlugin.cs
+++ RefreshMarket/RefreshMarketPlugin.cs
@@ -14,7 +14,6 @@ namespace RefreshMarket
  
         private static ManualLogSource logger;
 
-        private static bool NeedRefresh = false;
         void Awake()
         {
             logger = Logger;
@@ -24,6 +23,19 @@ namespace RefreshMarket
             Harmony.CreateAndPatchAll(typeof(RefreshMarketPlugin));
 
             logger.Infomation($"刷新市场 Mod V{version} 已加载.");
+
+            
+        }
+
+        void Update()
+        {
+            var key = RefreshMarketToogle.Value;
+            if (key.IsDown())
+            {
+                shouldRefreshMarket = !shouldRefreshMarket;
+
+                Logger.LogInfo($"刷新市场功能 {(shouldRefreshMarket ? "已开启" : "已关闭")}");
+            }
         }
 
 
@@ -31,13 +43,13 @@ namespace RefreshMarket
         [HarmonyPatch(typeof(Starbase), "UpdateStore")]
         public static bool UpdateStorePatch(Starbase __instance, float deltaTime)
         {
-            if (ShouldRefreshMarket.Value)
+            if (shouldRefreshMarket)
             {
                 var oldValue = Traverse.Create(__instance).Field("storeSpawnTimer").GetValue();
 
                 Traverse.Create(__instance).Field("storeSpawnTimer").SetValue((float)0);
 
-                logger.Infomation($"将补货时间从 {oldValue} 设为 0");
+                logger.Infomation($"将补货时间剩余从 {oldValue} 设为 0");
             }
 
             
@@ -50,7 +62,7 @@ namespace RefreshMarket
         public static void SetStarbaseStoreCapacityMultiplier(ref int __result)
         {
             var result = __result * StoreGoodsMultiplier.Value;
-            logger.Infomation($"市场容量 {__result} => {result}");
+            logger.Infomation($"扩大市场容量 {__result} => {result}");
 
             __result = result;
         }
@@ -58,13 +70,15 @@ namespace RefreshMarket
 
         #region Configuration
         private static ConfigEntry<int> StoreGoodsMultiplier;
-        private static ConfigEntry<bool> ShouldRefreshMarket;
+        private static ConfigEntry<KeyboardShortcut> RefreshMarketToogle;
+        private static bool shouldRefreshMarket = false;
+        private KeyboardShortcut _toggleShortcut;
         private void AddConfiguration()
         {
             StoreGoodsMultiplier = Config.Bind("商品数量乘数", "Store Multiplier", 32, new ConfigDescription("Description", new AcceptableValueRange<int>(0, 100)));
 
-            ShouldRefreshMarket = Config.Bind("刷新市场", "ShouldRefreshMarket", false, "刷新市场"); 
-        
+
+            RefreshMarketToogle = Config.Bind("刷新市场热键", "RefreshMarketHotKey", new KeyboardShortcut(KeyCode.F8), "刷新市场热键"); 
         }
 
         #endregion
